924fce46ee055c4b1a55fcf3b6b6569f012761c9
Ichsan Hardy <ichsanhardy@gmail.com>
2025-04-24 12:48:37 +0700

    Menghilangkan parameter Rainfall untuk SPARING

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ import psycopg2
 from psycopg2 import sql
 from datetime import datetime
 
-# Konfigurasi koneksi database
 DB_CONFIG = {
     "host": "localhost",
     "database": "wqms_db",
@@ -11,22 +10,22 @@ DB_CONFIG = {
     "port": 5433
 }
 
-# Fungsi untuk menyimpan data ke database
-def insert_data(total_rainfall, ph, tss, nh3n, depth, flow, cod):
+def insert_data(ph, tss, nh3n, depth, flow, cod):
     query = """
-    INSERT INTO sensor_datas (datetime, rain, ph, tss, nh3n, depth, flow, cod)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+    INSERT INTO sensor_datas (datetime, ph, tss, nh3n, depth, flow, cod)
+    VALUES (%s, %s, %s, %s, %s, %s, %s);
     """
     try:
         conn = psycopg2.connect(**DB_CONFIG)
         cursor = conn.cursor()
         timestamp = datetime.now()
 
-        cursor.execute(query, (timestamp, total_rainfall, ph, tss, nh3n, depth, flow, cod))
+        cursor.execute(query, (timestamp, ph, tss, nh3n, depth, flow, cod))
         conn.commit()
 
         cursor.close()
         conn.close()
-        print(f"[INFO] Data berhasil dimasukkan: {timestamp}, {total_rainfall}, {ph}, {tss}, {nh3n}, {cod}, {depth}, {flow}")
+        print(f"[INFO] Data berhasil dimasukkan: {timestamp}, {ph}, {tss}, {nh3n}, {cod}, {depth}, {flow}")
     except Exception as e:
         print(f"[ERROR] Gagal memasukkan data ke database: {e}")
+
