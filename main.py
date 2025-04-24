0029296074a137c081e9c4d23d42a63fc6ec9a1b
Ichsan Hardy <ichsanhardy@gmail.com>
2025-04-24 12:49:45 +0700

    Merevisi parameter yang dikuru sesuai dengan peraturan SPARING

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import time
 from at500 import get_at500_data
 from mace import get_mace_data
-from rain import get_rainfall, cleanup
 from spectro import read_modbus_tcp
 from database import insert_data
+from datetime import datetime
+
+# Ganti nilai interval sesuai kebutuhan (dalam menit)
+interval = 1  # contoh: 1, 2, atau 3
 
 try:
     while True:
-        tipping_count, total_rainfall = get_rainfall()
-        ph, tss, nh3n = get_at500_data()
-        bat, depth, flow = get_mace_data()
-        cod = read_modbus_tcp()
+        now = datetime.now()
+        if now.minute % interval == 0 and now.second == 0:
+            ph, tss, nh3n = get_at500_data()
+            bat, depth, flow = get_mace_data()
+            cod = read_modbus_tcp()
 
-        print("\n=== SENSOR DATA ===")
-        print(f"Jumlah tipping: {tipping_count}")
-        print(f"Total curah hujan: {total_rainfall:.3f} mm")
-        print(f"pH: {ph}, TSS: {tss}, NH3-N: {nh3n}")
-        print(f"Bat: {bat}, Depth: {depth}, Flow: {flow}")
-        print(f"COD: {cod} mg/L")
-        print("===================")
+            print("\n=== SENSOR DATA ===")
+            print(f"pH: {ph}, TSS: {tss}, NH3-N: {nh3n}")
+            print(f"Bat: {bat}, Depth: {depth}, Flow: {flow}")
+            print(f"COD: {cod} mg/L")
+            print("===================")
 
-        # Simpan data ke database
-        insert_data(total_rainfall, ph, tss, nh3n, depth, flow, cod)
+            # Simpan data ke database
+            insert_data(ph, tss, nh3n, depth, flow, cod)
 
-        time.sleep(2)
+            # Tunggu satu detik untuk menghindari pengulangan dalam satu menit
+            time.sleep(1)
+        else:
+            # Cek lagi setiap detik
+            time.sleep(1)
 
 except KeyboardInterrupt:
     print("\nProgram dihentikan oleh pengguna.")
-finally:
-    cleanup()
